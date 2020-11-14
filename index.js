87e07da3923d70f8ef72ee86b915099956d83d62
Matt Albinson <mattalbinson@berkeley.net>
2020-11-14 17:51:02 +0000

    enhance

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express()
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.set('view engine', ejs)
 app.use(express.static("public"))
 
